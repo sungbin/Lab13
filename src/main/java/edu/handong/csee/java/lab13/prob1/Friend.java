0d554d4d34a84695be25c583b69eb9a41548ad35
sungbin <21700613@handong.edu>
2018-04-26 11:17:33 +0900

    prob1 complete

---
+++ src/main/java/edu/handong/csee/java/lab13/prob1/Friend.java
@@ -0,0 +1,9 @@
+package edu.handong.csee.java.lab13.prob1;
+
+public class Friend {
+	public void JustFriend() {
+		System.out.println("Just Friend!");
+		
+	}
+
+}
---
+++ src/main/java/edu/handong/csee/java/lab13/prob1/Main.java
@@ -0,0 +1,17 @@
+package edu.handong.csee.java.lab13.prob1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Friend friend1 = new Friend();
+		SchoolFriend friend2 = new SchoolFriend();
+		ClassFriend friend3 = new ClassFriend();
+		
+		InstanceOf.WhatFriend(friend1);
+		InstanceOf.WhatFriend(friend2);
+		InstanceOf.WhatFriend(friend3);
+
+	}
+
+}
