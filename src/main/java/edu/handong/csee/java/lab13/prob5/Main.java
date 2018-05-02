1e9be22e95d8670066df7470de2b6b466d0a67a9
sungbin <21700613@handong.edu>
2018-05-02 22:41:28 +0900

    complete single line comment prob5

--- src/main/java/edu/handong/csee/java/lab13/prob5/Main.java
+++ src/main/java/edu/handong/csee/java/lab13/prob5/Main.java
@@ -1,85 +1,85 @@
-package edu.handong.csee.java.lab13.prob5;
-import java.util.Scanner;
+package edu.handong.csee.java.lab13.prob5; //the file is in this package
+import java.util.Scanner; //include Scanner class
 
-public class Main {
-	static Scanner mykey = new Scanner(System.in);
+public class Main { //declare public type Main class
+	static Scanner mykey = new Scanner(System.in); //make static instance of Scanner
 
-	public static void main(String[] args) {
-		Main ma = new Main();
+	public static void main(String[] args) { //declare main method
+		Main ma = new Main(); //make instance of Main
 		
-		int n1, n2 = 0;
-		int m1, m2 = 0;
-		System.out.print("Enter row size(maximum 5):");
-		n1 = mykey.nextInt();
-		System.out.print("Enter column size(maximum 5):");
-		n2 = mykey.nextInt();
+		int n1, n2 = 0; //declare integer type variable n1, n2
+		int m1, m2 = 0; //declare integer type variable n1, n2
+		System.out.print("Enter row size(maximum 5):"); //print the sentence
+		n1 = mykey.nextInt(); //scan one Integer from user
+		System.out.print("Enter column size(maximum 5):"); //print the sentence
+		n2 = mykey.nextInt(); //scan one Integer from user
 		
-		System.out.print("Enter row size(maximum 5):");
-		m1 = mykey.nextInt();
-		System.out.print("Enter column size(maximum 5):");
-		m2 = mykey.nextInt();
+		System.out.print("Enter row size(maximum 5):"); //print the sentence
+		m1 = mykey.nextInt(); //scan one Integer from user
+		System.out.print("Enter column size(maximum 5):"); //print the sentence
+		m2 = mykey.nextInt(); //scan one Integer from user
 		
-		int[][] first = new int[n1][n2];
-		int second[][] = new int[m1][m2];
+		int[][] first = new int[n1][n2]; //declare 2 Dimensional Array
+		int second[][] = new int[m1][m2]; //declare 2 Dimensional Array
 
-		System.out.print("Enter list1: ");
-		ma.inputDataToArrays(first);
-		System.out.print("Enter list2: ");
-		ma.inputDataToArrays(second);
+		System.out.print("Enter list1: "); //print the sentence
+		ma.inputDataToArrays(first); //execute inputDataToArrays
+		System.out.print("Enter list2: "); //print the sentence
+		ma.inputDataToArrays(second); //execute inputDataToArrays
 		
-		ma.printDataToArrays(first);
-		ma.printDataToArrays(second);
+		ma.printDataToArrays(first); //execute printDataToArrays
+		ma.printDataToArrays(second); //execute printDataToArrays
 		
-		if(ma.Equals(first,second))
+		if(ma.Equals(first,second))  //if result value of Equals method is true,
 		{
-			System.out.println("The two arrays are approximately identical");
+			System.out.println("The two arrays are approximately identical"); //print the sentence
 		}
-		else
+		else //else
 		{
-			System.out.println("The two arrays are not identical");
+			System.out.println("The two arrays are not identical"); //print the sentence
 		}
 	}
 	
-	public void inputDataToArrays(int array[][])
+	public void inputDataToArrays(int array[][]) //declare public type inputDataToArrays, return nothing, parameter is integer
 	{
-		for (int i = 0; i < array.length; i++) 
+		for (int i = 0; i < array.length; i++)  //execute for-statement
 	      {
-	            for (int j = 0; j < array[i].length; j++)
+	            for (int j = 0; j < array[i].length; j++) // execute for-statement
 	            {
-	                array[i][j] = mykey.nextInt();
+	                array[i][j] = mykey.nextInt(); //scan value and put to array
 	            }
 	      }
 
 	}
-	public void printDataToArrays(int array[][])
+	public void printDataToArrays(int array[][]) //declare public type printDataToArrays, return nothing, parameter is integer
 	{
-		for (int i = 0; i < array.length; i++) 
+		for (int i = 0; i < array.length; i++) //execute for-statement
 	      {
-	            for (int j = 0; j < array[i].length; j++)
+	            for (int j = 0; j < array[i].length; j++) //execute for-statement
 	            {
-	                System.out.print(array[i][j]+ " ");
+	                System.out.print(array[i][j]+ " ");//print the sentence
 	            }
-	            System.out.println();
+	            System.out.println();//print one enter
 	      }
-		System.out.println();
+		System.out.println(); //print one enter
 	}
 	
-	public boolean Equals(int first[][], int second[][]) {
-		int count =0;
-		if(first.length != second.length)
-			return false;
-		for(int i = 0; i < first.length; i++) 
+	public boolean Equals(int first[][], int second[][]) { //declare public type printDataToArrays, return type is boolean, parameter is integer
+		int count =0; //declare integer variable
+		if(first.length != second.length) //if Array's length is not equal
+			return false; //return false
+		for(int i = 0; i < first.length; i++)  //execute for-statement
 	    {
-	           for (int j = 0; j < first[i].length; j++)
+	           for (int j = 0; j < first[i].length; j++) //execute for-statement
 	           {
-	           if (first[i][j] != second[i][j])
-	                   count++;
+	           if (first[i][j] != second[i][j]) //if first[i][j] equals second[i][j]
+	                   count++; //count plus 1
 	           }
 	    }
-		if(count <= 3)
-	         return true;
-	    else
-	         return false;
+		if(count <= 3) //if count is under or equals 3
+	         return true;  //return true
+	    else //else
+	         return false; //return false
 		
 	}
 }
