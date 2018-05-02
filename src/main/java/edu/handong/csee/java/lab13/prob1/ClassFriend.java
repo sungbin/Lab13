7cec2572099d1e9fd5b9a668597521332df0def4
sungbin <21700613@handong.edu>
2018-05-02 20:08:28 +0900

    add singleline comment classFriend and Friend

--- src/main/java/edu/handong/csee/java/lab13/prob1/ClassFriend.java
+++ src/main/java/edu/handong/csee/java/lab13/prob1/ClassFriend.java
@@ -1,9 +1,9 @@
-package edu.handong.csee.java.lab13.prob1;
+package edu.handong.csee.java.lab13.prob1; // the file is in this package
 
-public class ClassFriend extends SchoolFriend {
-	public void classFriend()
+public class ClassFriend extends SchoolFriend { //declare public type ClassFriend, inheritance of SchoolFriend
+	public void classFriend() //declare public type classFriend method, return nothing,
 	{
-		System.out.println("Class Friend");
+		System.out.println("Class Friend"); //print the sentence
 	}
 
 }
--- src/main/java/edu/handong/csee/java/lab13/prob1/Friend.java
+++ src/main/java/edu/handong/csee/java/lab13/prob1/Friend.java
@@ -1,8 +1,8 @@
-package edu.handong.csee.java.lab13.prob1;
+package edu.handong.csee.java.lab13.prob1; //the file is in this package
 
-public class Friend {
-	public void JustFriend() {
-		System.out.println("Just Friend!");
+public class Friend { //declare public type friend class.
+	public void JustFriend() { //declare public type JustFriend method, return nothing
+		System.out.println("Just Friend!"); //print the sentence
 		
 	}
 
